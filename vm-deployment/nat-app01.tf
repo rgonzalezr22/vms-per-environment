c9f1915559b9939b00e4de46a70bab406dc1b1a9
Ricardo Gonzalez Rodriguez <104998714+rgonzalezr22@users.noreply.github.com>
2023-08-17 12:30:01 -0600

    Update nat-app01.tf

--- vm-deployment/nat-app01.tf
+++ vm-deployment/nat-app01.tf
@@ -42,8 +42,8 @@ module "nat-app01" {
     guest_dns_suffix        = "flexlab.local"
     guest_domain            = "flexlab.local"
        
-    guest_category_name     = "Analytics-App"
-    guest_tag_name          = "Productive-Linux"
+    guest_category_name     = "environment"
+    guest_tag_name          = "prod"
 }
 
 output "nat-app01-VM-ip" {
