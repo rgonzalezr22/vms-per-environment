27d301d05c6c7787606f7007d5c69b316fc8afd4
Ricardo Gonzalez Rodriguez <104998714+rgonzalezr22@users.noreply.github.com>
2023-08-17 12:23:08 -0600

    Update server3.tf

--- vm-deployment/server3.tf
+++ vm-deployment/server3.tf
@@ -30,26 +30,18 @@ module "nat-web01" {
     # -------------------------------------------------------------
     guest_template          = "Ubuntu-20-04-Template"
     guest_template_folder   = "Templates"
-    guest_vm_name           = "nat-web01"
+    guest_vm_name           = "server3"
     guest_vcpu              = "1"
     guest_memory            = "4096"
     guest_disk0_size        = "30"
-    guest_network           = "TF-Segment-Web"
-    guest_ipv4_address      = "10.10.20.10"
+    guest_network           = "TF-LB"
+    guest_ipv4_address      = "172.70.10.12"
     guest_ipv4_netmask      = "24"
-    guest_ipv4_gateway      = "10.10.20.1"
-    guest_dns_servers       = "10.10.20.1"
+    guest_ipv4_gateway      = "172.70.10.1"
+    guest_dns_servers       = "8.8.8.8"
     guest_dns_suffix        = "flexlab.local"
     guest_domain            = "flexlab.local"
        
-    guest_category_name     = "Analytics-App"
-    guest_tag_name          = "Productive-Linux"
+    guest_category_name     = "environment"
+    guest_tag_name          = "prod"
 }
-
-output "nat-web01-VM-ip" {
-	value = module.nat-web01.VM-ip
-}
-output "nat-web01-VM-guest-ip" {
-	value = module.nat-web01.VM-guest-ip
-}
-
