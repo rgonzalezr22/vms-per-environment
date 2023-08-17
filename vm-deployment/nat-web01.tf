05b9742348f2ec95ee3e7ffdc163c7e6f4b692ab
Ricardo Gonzalez Rodriguez <104998714+rgonzalezr22@users.noreply.github.com>
2023-08-17 03:05:45 -0600

    Update nat-web01.tf

--- vm-deployment/nat-web01.tf
+++ vm-deployment/nat-web01.tf
@@ -19,8 +19,8 @@ module "nat-web01" {
     # -------------------------------------------------------------
     # INFRASTRUCTURE - VMware vSphere vCenter settings 
     # -------------------------------------------------------------
-    vsphere_datacenter   = "EAST-DC"
-    vsphere_cluster      = "EAST-Cluster"
+    vsphere_datacenter   = "Datacenter"
+    vsphere_cluster      = "los-angeles-cluster"
     vsphere_datastore    = "vsanDatastore"
     vsphere_folder       = "Production"
     vsphere_sub_folder   = "Applications"
@@ -28,22 +28,22 @@ module "nat-web01" {
     # -------------------------------------------------------------
     # GUEST - VMware vSphere VM settings 
     # -------------------------------------------------------------
-    guest_template          = "CentOS7-Template-noX"
+    guest_template          = "Ubuntu-20-04-Template"
     guest_template_folder   = "Templates"
     guest_vm_name           = "nat-web01"
     guest_vcpu              = "1"
     guest_memory            = "4096"
     guest_disk0_size        = "30"
-    guest_network           = "lab-mgmt"
-    guest_ipv4_address      = "192.168.111.49"
+    guest_network           = "TF-Segment-Web"
+    guest_ipv4_address      = "10.10.20.10"
     guest_ipv4_netmask      = "24"
-    guest_ipv4_gateway      = "192.168.111.1"
-    guest_dns_servers       = "192.168.111.111"
+    guest_ipv4_gateway      = "10.10.20.1"
+    guest_dns_servers       = "10.10.20.1"
     guest_dns_suffix        = "flexlab.local"
     guest_domain            = "flexlab.local"
        
-    guest_category_name     = "CUSTOM-CATEGORY"
-    guest_tag_name          = "CUSTOM-TAG"
+    guest_category_name     = "web"
+    guest_tag_name          = "server"
 }
 
 output "nat-web01-VM-ip" {
