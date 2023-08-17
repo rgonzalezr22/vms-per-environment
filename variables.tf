ecfb322e40e622d27c02d62d76db25386190caac
Ricardo Gonzalez Rodriguez <104998714+rgonzalezr22@users.noreply.github.com>
2023-08-17 00:21:15 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,16 +12,19 @@
 variable "provider_vsphere_host" {
     description = "vCenter server FQDN or IP"
     type        = string
+    default     = vcsa-196337.a5a79b67.us-west2.gve.goog
 }
 
 variable "provider_vsphere_user" {
     description = "vSphere username to use to connect to the environment"
     type        = string
+    default     = CloudOwner@gve.local
 }
 
 variable "provider_vsphere_password" {
     description = "vSphere password"
     type        = string
+    default     = TMcaYhQi=kwjXU0W
 }
 
 variable "provider_vsphere_unverified_ssl" {
