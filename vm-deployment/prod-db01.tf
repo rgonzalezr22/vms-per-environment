ab5e26cce50f986e788d0ddfea83d24d87a4a09f
Ricardo Gonzalez Rodriguez <104998714+rgonzalezr22@users.noreply.github.com>
2023-08-17 12:08:14 -0600

    Update prod-db01.tf

--- vm-deployment/prod-db01.tf
+++ vm-deployment/prod-db01.tf
@@ -34,11 +34,11 @@ module "nat-db01" {
     guest_vcpu              = "1"
     guest_memory            = "4096"
     guest_disk0_size        = "30"
-    guest_network           = "TF-Segment-DB"
+    guest_network           = "TF-PROD"
     guest_ipv4_address      = "10.10.40.11"
     guest_ipv4_netmask      = "24"
     guest_ipv4_gateway      = "10.10.40.1"
-    guest_dns_servers       = "10.10.40.1"
+    guest_dns_servers       = "8.8.8.8"
     guest_dns_suffix        = "flexlab.local"
     guest_domain            = "flexlab.local"
        
